105d897407dbe6decd95e180a497da73ddb89e8d
faruq <farooq6116@gmail.com>
2025-06-20 14:47:40 +0100

    update changes of js in index and config files

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 console.log("confif.js");
 console.log("version2 updated, modified further");
 console.log("version 11");
+console.log("version 13");
--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 console.log("confif.js");
 console.log("version2");
+console.log("version 13");
